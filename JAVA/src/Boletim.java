4b60764fb7979858878f28aaf0b6bf9467cf9863
Michel Lima <michellimazn@gmail.com>
2024-08-29 20:29:48 -0300

    Criando um projeto com tipo de variaveis

---
+++ JAVA/src/Boletim.java
@@ -0,0 +1,14 @@
+public class Boletim {
+    public static void main(String[] args) {
+        int mediafinal = 8;
+
+        if (mediafinal < 6){
+            System.out.println("Reprovado");
+        } else if (mediafinal == 6){
+            System.out.println("Prova Minerva");
+        } else {
+            System.out.println("aprovado");
+        }
+    }
+    
+}
